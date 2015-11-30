57c3151d813fa824bf2ca696c1c2c0aaef743158
Alejandro Soret <soret_40@hotmail.com>
2015-11-30 13:28:05 +0100

    After passing a simple text file as a parameter, we then output its content one line at a time.

--- My-First-Windows-Application/My-First-Windows-Application.cpp
+++ My-First-Windows-Application/My-First-Windows-Application.cpp
@@ -4,6 +4,8 @@
 #include "stdafx.h"
 
 
+void openFile( WCHAR* argv1 );
+
 int wmain( int argc, WCHAR* argv[] )
 {
 	wprintf( L"\n------------------------\n\n" );
@@ -13,7 +15,40 @@ int wmain( int argc, WCHAR* argv[] )
 		wprintf( L"Argument %d: %s \n", i, argv[ i ] );
 	}
 
+	openFile( argv[ 1 ] );
+
 	wprintf( L"\n\n------------------------" );
 
 	return 0;
+}
+
+void openFile( WCHAR* argv1 )
+{
+	wprintf( L"\nThe argument we're interested in is the second one: %s\n\n", argv1 );
+
+	FILE* stream;
+	errno_t err;
+	err = _wfopen_s( &stream, argv1, L"r" );
+
+	if( err == 0 )
+	{
+		wprintf( L"The file %s has been opened.\n\n", argv1 );
+
+		WCHAR str[ 100 ];
+
+		//if (fgetws(str, 100, stream) == NULL) {	wprintf(L"read error or in \"fgetws\"\n");	}
+
+		while( !feof( stream ) )
+		{
+			fgetws( str, 100, stream );
+			wprintf( L"%s", str );
+		}
+	}
+	else
+	{
+		wprintf( L"Error code: %d \n", err );
+		return;
+	}
+
+	fclose( stream );
 }
